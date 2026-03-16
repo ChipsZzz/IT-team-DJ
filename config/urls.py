2b3728c4b667682851f747050c1fdc06161a3a14
ChipsZzz <3149144l@student.gla.ac.uk>
2026-03-16 21:06:51 +0000

    Enable media files serving in production

--- config/urls.py
+++ config/urls.py
@@ -47,9 +47,8 @@ urlpatterns = [
         auth_views.LogoutView.as_view(),
         name="logout"
     ),
-
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Enable media file serving (for Render deployment)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
